b9455deef8fb2e60867be042803ca8a6552c2135
MuhammadBilal <muhammad.bilal@devsinc.com>
2020-08-19 11:31:55 +0500

    Implement advanced login

--- Gemfile
+++ Gemfile
@@ -63,3 +63,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rails-controller-testing'
 gem 'hirb'
+# gem 'bootstrap'
+# gem 'sass'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     # debugger
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
     else
     render 'new'
     end
   end
+  
 
   private
   
